ac800cded1bc72a92553161c8b780eb4714850a7
trex-ayush <singhayush.sa81@gmail.com>
2026-01-11 21:42:44 +0530

    fix: configure CORS with environment-based origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
